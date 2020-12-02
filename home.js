9d99b54500052a9bde42e4ba6cb161d7ee90d0eb
Amitav-Krishna <amtiavkrishna2011@gmail.com>
2020-12-02 17:24:49 -0500

    Add spaniel image

---
+++ home.js
@@ -0,0 +1,9 @@
+console.log('hello motou');
+alert('you smell like cheese');
+//poop
+
+var fat = 'your momma';
+console.log(fat);
+
+var someNumber = 100020003000400005000600070008000900001000000*2;
+console.log(someNumber);
